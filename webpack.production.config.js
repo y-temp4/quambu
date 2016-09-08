c76bb01f158aa9e8f35fd8921f1b714a53503e4f
y-temp4 <y.temp4@gmail.com>
2016-09-08 21:44:03 +0900

    [WIP] add production setting of webpack

---
+++ webpack.production.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3333',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        // loader: 'babel',
+        loaders: ['react-hot', 'babel-loader'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.template.html',
+      filename: 'index.html'
+    })
+  ],
+};
