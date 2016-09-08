47d20f8ddd282df492b1ce1db392968bc666ced9
y-temp4 <y.temp4@gmail.com>
2016-09-08 14:13:45 +0900

    show items via mock data

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
+
+const ENDPOINT = 'https://qiita.com/api/v1';
+const NEW_ARTICLES_URI = ENDPOINT + '/items';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('../../mock/items.json')
+    // fetch(NEW_ARTICLES_URI)
+      .then((response) => response.json() )
+      .then((json) => { this.setState({items: json})
+        console.log('parsed json', this.state.items)
+      }).catch((ex) => { console.log('parsing failed', ex) })
   }
 
   render() {
     return (
-      <h1>hello!!!!!!</h1>
+      <div>
+        <h1>New Articles</h1>
+        <ol>
+          {
+            this.state.items.map((item) => {
+              return (
+                <li key={item.id}>
+                  <a href={item.url} target="_blank">{item.title}</a>
+                </li>
+              )
+            })
+          }
+        </ol>
+      </div>
     );
   }
 }
