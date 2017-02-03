ed755fa1419758d1bd364297d5a6a77b1f204a7b
y-temp4 <y.temp4@gmail.com>
2017-02-03 17:51:47 +0900

    [WIP] Add sample setting layout

--- src/components/side-menu.jsx
+++ src/components/side-menu.jsx
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 import Drawer from 'react-toolbox/lib/drawer';
+import Input from 'react-toolbox/lib/input';
+import Slider from 'react-toolbox/lib/slider';
 
 export default class SideMenu extends Component {
   constructor(props) {
     super(props);
 
-    this._handleToggle = this._handleToggle.bind(this)
+    this.state = {
+      bookmark_count: 0,
+      stock_count: 0
+    };
   }
 
   _handleToggle() {
     this.props.toggleDrower();
   }
 
+  _handleChange(slider, value) {
+    const newState = {};
+    newState[slider] = value;
+    this.setState(newState);
+  }
+
   render() {
     return (
-      <Drawer active={this.props.active} onOverlayClick={this._handleToggle}>
-        <h5>This is your Drawer.</h5>
-        <p>You can embed any content you want, for example a Menu.</p>
+      <Drawer
+        active={this.props.active}
+        onOverlayClick={this._handleToggle.bind(this)}>
+        <section style={{padding: 10}}>
+          <p>各種設定</p>
+          <Input type='text' label='ユーザーID'/>
+          <p>ブックマーク数</p>
+          <Slider
+            value={this.state.bookmark_count}
+            onChange={this._handleChange.bind(this, 'bookmark_count')}
+            step={1}
+            max={100}
+            pinned
+            editable/>
+          <p>ストック数</p>
+          <Slider
+            value={this.state.stock_count}
+            onChange={this._handleChange.bind(this, 'stock_count')}
+            step={1}
+            max={100}
+            pinned
+            editable/>
+        </section>
       </Drawer>
     );
   }
